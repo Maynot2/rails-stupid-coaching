020722c14b92f6957402d2cd1aa5ce044f88e3a8
paul manot <paulmanot@gmail.com>
2017-02-13 14:57:47 +0100

    finish exo stupid coaching rails

---
+++ app/controllers/coaching_controller.rb
@@ -0,0 +1,32 @@
+class CoachingController < ApplicationController
+  def answer
+    @question = params[:question]
+    @answer = coach_answer_enhanced(@question)
+  end
+
+  def ask
+  end
+
+  def coach_answer(your_message)
+  # TODO: return coach answer to your_message
+  if your_message == "I am going to work right now!"
+    return ""
+  elsif your_message.include?("?")
+    "Silly question, get dressed and go to work!"
+  else
+    return "I don't care, get dressed and go to work!"
+  end
+end
+
+def coach_answer_enhanced(your_message)
+  # TODO: return coach answer to your_message, with additional custom rules of yours!
+  if your_message == "I AM GOING TO WORK RIGHT NOW!"
+    return ""
+  elsif your_message == your_message.upcase
+    return "I can feel your motivation!" + " " + coach_answer(your_message).to_s
+  else
+    return coach_answer(your_message).to_s
+  end
+end
+
+end
